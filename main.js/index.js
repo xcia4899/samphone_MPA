fc4fb3d3a139b71e1078a700f8c17a6da8bde173
xcia4899 <xcia48996531@gmail.com>
2025-07-02 15:13:53 +0800

    gsap使用測試

--- main.js/index.js
+++ main.js/index.js
@@ -1,3 +1,5 @@
+
+
 export const indexdrop = {
   data() {
     return {
@@ -9,10 +11,27 @@ export const indexdrop = {
       if (event.animationName === "fadeOut") {
         this.introPlayed = true;
         document.body.classList.remove("no-scroll");
+
+        this.$nextTick(() => {
+          gsap.to(".row-01", {
+            scrollTrigger: {
+              trigger: ".row-01",
+              start: "top top",
+              end: "+=2000",  // 滾動距離 2000px 後解除固定
+              scrub: true,
+              pin: true,
+              markers: true, // 開發時用，記得完成後刪掉
+            },
+          });
+        });
+
       }
     }
   },
   mounted() {
     document.body.classList.add("no-scroll");
+    gsap.registerPlugin(ScrollTrigger);
+    // 這裡不要執行 .block1還沒出現
   }
+
 };
