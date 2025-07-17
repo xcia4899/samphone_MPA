be9a6c10d7bd2d6ee486a3ee2c7e1ce1de9d05de
xcia4899 <xcia48996531@gmail.com>
2025-07-17 15:12:17 +0800

    大致上完成+準備製作RWD

--- main.js/index.js
+++ main.js/index.js
@@ -7,44 +7,44 @@ export const indexdrop = {
 
         {
           title: "鑄造",
-          image: "../images/row4/IMG_7743.jpg",
+          image: "../images/row4/IMG_7748.jpg",
           imagedetal: "../images/row4/IMG_7743.jpg",
           content: "從一塊塊銅板開始，經由模具精準裁切，打造出樂器的主要金屬部件。這是每一支樂器成形的第一步，承載著音樂誕生的可能性。"
         },
         {
           title: "焊接",
           image: "../images/row4/IMG_7785.jpg",
-          imagedetal: "../images/row4/IMG_7743.jpg",
+          imagedetal: "../images/row4/IMG_7742.jpg",
           content: "透過火槍高溫焊接，將各段銅件準確接合，確保管身結構穩固與氣密性。焊接時的溫度控制與接縫精度，直接影響樂器的共鳴特性與音色表現。"
         },
         {
           title: "研磨",
           image: "../images/row4/IMG_7774.jpg",
-          imagedetal: "../images/row4/IMG_7743.jpg",
+          imagedetal: "../images/row4/IMG_7766.jpg",
           content: "透過機械或手工方式打磨金屬表面，初步去除毛邊與不規則處，使其平整細緻。這一步雖未追求光澤，但為後續的拋光與表面處理提供關鍵基礎。"
         },
         {
           title: "刻花",
           image: "../images/row4/IMG_7393.jpg",
-          imagedetal: "../images/row4/IMG_7743.jpg",
+          imagedetal: "../images/row4/IMG_7411-02.jpg",
           content: "以手工方式在金屬表面雕刻紋樣，提升樂器的視覺感，同時展現品牌識別。刻花需精準掌握力度與線條流暢度，確保美觀性並且不影響結構。"
         },
         {
           title: "拋光",
           image: "../images/row4/IMG_7782.jpg",
-          imagedetal: "../images/row4/IMG_7743.jpg",
+          imagedetal: "../images/row4/IMG_7782.jpg",
           content: "使用拋光輪與研磨膏等工具，細緻修整金屬表面，使其更加平滑亮麗。本工序銜接研磨後進行，為後續電鍍或塗裝提供穩定且均勻的基礎層。"
         },
         {
           title: "上漆",
-          image: "../images/row4/IMG_7411.jpg",
-          imagedetal: "../images/row4/IMG_7743.jpg",
+          image: "../images/row4/IMG_1905.jpg",
+          imagedetal: "../images/row4/IMG_1905.jpg",
           content: "依據設計需求，噴塗透明或彩色漆層，形成一層均勻且堅固的保護膜，有效防止金屬表面氧化與腐蝕，同時提升樂器的耐用性與使用壽命。"
         },
         {
           title: "組裝",
           image: "../images/row4/IMG_9475.jpg",
-          imagedetal: "../images/row4/IMG_7743.jpg",
+          imagedetal: "../images/row4/IMG_0539.jpg",
           content: "將各部件依照設計精準結合，包括管件、按鍵、墊片與連動機構等。每一個接合點與機構配置，皆影響樂器的操作手感與氣密穩定性，是實用性與耐用性的關鍵。"
         },
         {
@@ -55,7 +55,7 @@ export const indexdrop = {
         },
         {
           title: "品檢和包裝",
-          image: "../images/row4/IMG_4425.jpg",
+          image: "../images/row4/IMG_7411.jpg",
           imagedetal: "../images/row4/IMG_7743.jpg",
           content: "每件樂器在出廠前皆經嚴格品檢，涵蓋外觀、結構完整性及音準測試，確保達到高品質標準。通過品檢後，樂器會被妥善包裝，使用防護材料加以保護，並安放於專用樂器箱內，確保產品能以最佳狀態安全送達客戶手中。"
         }
@@ -281,6 +281,15 @@ export const indexdrop = {
             {
               animation: { x: "-84%" },
               scroll: { start: "top 20%", end: "+=4600", },
+            },
+          ],
+        },
+        {
+          selector: ".row-05",
+          steps: [
+            {
+              animation: {},
+              scroll: { start: "top top", end: "+=1000", pin: true },
             },
           ],
         },
